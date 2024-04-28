664b2f95ee5387d62500cffd22cf19849a62ca5c
shaikat213 <shaikat213@gmail.com>
2024-04-28 22:56:49 +0600

    Add project files.

---
+++ src/StoredProcedureDemo.DbMigrator/DbMigratorHostedService.cs
@@ -0,0 +1,45 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using StoredProcedureDemo.Data;
+using Serilog;
+using Volo.Abp;
+using Volo.Abp.Data;
+
+namespace StoredProcedureDemo.DbMigrator
+{
+    public class DbMigratorHostedService : IHostedService
+    {
+        private readonly IHostApplicationLifetime _hostApplicationLifetime;
+
+        public DbMigratorHostedService(IHostApplicationLifetime hostApplicationLifetime)
+        {
+            _hostApplicationLifetime = hostApplicationLifetime;
+        }
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            using (var application = AbpApplicationFactory.Create<StoredProcedureDemoDbMigratorModule>(options =>
+            {
+                options.UseAutofac();
+                options.Services.AddLogging(c => c.AddSerilog());
+                options.Services.AddDataMigrationEnvironment();
+            }))
+            {
+                application.Initialize();
+
+                await application
+                    .ServiceProvider
+                    .GetRequiredService<StoredProcedureDemoDbMigrationService>()
+                    .MigrateAsync();
+
+                application.Shutdown();
+
+                _hostApplicationLifetime.StopApplication();
+            }
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken) => Task.CompletedTask;
+    }
+}
---
+++ test/StoredProcedureDemo.HttpApi.Client.ConsoleTestApp/StoredProcedureDemoConsoleApiClientModule.cs
@@ -0,0 +1,16 @@
+﻿using Volo.Abp.Http.Client.IdentityModel;
+using Volo.Abp.Modularity;
+using Volo.Abp.Autofac;
+
+namespace StoredProcedureDemo.HttpApi.Client.ConsoleTestApp
+{
+    [DependsOn(
+        typeof(AbpAutofacModule),
+        typeof(StoredProcedureDemoHttpApiClientModule),
+        typeof(AbpHttpClientIdentityModelModule)
+        )]
+    public class StoredProcedureDemoConsoleApiClientModule : AbpModule
+    {
+        
+    }
+}
